a261dc3e38f5a43ab3e755e7a17eed0f5a0392ee
Haruaki Tamada <tamada@users.noreply.github.com>
2019-03-22 18:00:24 +0900

    Update database layout.

--- common/database.go
+++ common/database.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"time"
 )
 
@@ -25,9 +26,13 @@ type Repository struct {
 Group represents the groups of the Git repositories.
 */
 type Group struct {
-	Name        string   `json:"group_name"`
-	Description string   `json:"group_desc"`
-	Items       []string `json:"group_items"`
+	Name        string `json:"group_name"`
+	Description string `json:"group_desc"`
+}
+
+type Relation struct {
+	RepositoryID string `json:"repository_id"`
+	GroupName    string `json:"group_name"`
 }
 
 /*
@@ -37,27 +42,27 @@ type Database struct {
 	Timestamp    time.Time    `json:"last_modified"`
 	Repositories []Repository `json:"repositories"`
 	Groups       []Group      `json:"groups"`
+	Relations    []Relation   `json:"relations"`
 	Config       *Config      `json:"-"`
 }
 
-func (db *Database) countReposInGroups() map[string]int {
-	var repoFlags = map[string]int{}
-	for _, repo := range db.Repositories {
-		repoFlags[repo.ID] = 0
-	}
+func groupFrequencies(db *Database) map[string]int {
+	var groupMap = map[string]int{}
 	for _, group := range db.Groups {
-		for _, item := range group.Items {
-			repoFlags[item] = repoFlags[item] + 1
-		}
+		groupMap[group.Name] = 0
 	}
-	return repoFlags
+	for _, relation := range db.Relations {
+		groupMap[relation.GroupName] = groupMap[relation.GroupName] + 1
+	}
+	return groupMap
 }
 
-func (db *Database) pruneGroup() int {
+func pruneGroups(db *Database) int {
+	var groupMap = groupFrequencies(db)
 	var newGroups = []Group{}
 	var count = 0
 	for _, group := range db.Groups {
-		if len(group.Items) != 0 {
+		if groupMap[group.Name] != 0 {
 			newGroups = append(newGroups, group)
 		} else {
 			count++
@@ -68,12 +73,19 @@ func (db *Database) pruneGroup() int {
 	return count
 }
 
-/*
-Prune eliminates unnecessary repositories, and groups from db.
-*/
-func (db *Database) Prune() (int, int) {
-	var prunedGroupCount = db.pruneGroup()
-	var repoFlags = db.countReposInGroups()
+func repositoryFrequencies(db *Database) map[string]int {
+	var repoFlags = map[string]int{}
+	for _, repo := range db.Repositories {
+		repoFlags[repo.ID] = 0
+	}
+	for _, relation := range db.Relations {
+		repoFlags[relation.RepositoryID] = repoFlags[relation.RepositoryID] + 1
+	}
+	return repoFlags
+}
+
+func pruneRepositories(db *Database) int {
+	var repoFlags = repositoryFrequencies(db)
 	var repos = []Repository{}
 	var prunedReposCount = 0
 	for _, repo := range db.Repositories {
@@ -84,6 +96,15 @@ func (db *Database) Prune() (int, int) {
 		}
 	}
 	db.Repositories = repos
+	return prunedReposCount
+}
+
+/*
+Prune eliminates unnecessary repositories, and groups from db.
+*/
+func (db *Database) Prune() (int, int) {
+	var prunedGroupCount = pruneGroups(db)
+	var prunedReposCount = pruneRepositories(db)
 	return prunedGroupCount, prunedReposCount
 }
 
@@ -120,6 +141,9 @@ func (db *Database) CreateRepository(repoID string, path string, remotes []Remot
 	}
 	var repo = Repository{repoID, path, remotes}
 	db.Repositories = append(db.Repositories, repo)
+	sort.Slice(db.Repositories, func(i, j int) bool {
+		return db.Repositories[i].ID < db.Repositories[j].ID
+	})
 
 	return &repo, nil
 }
@@ -131,8 +155,12 @@ func (db *Database) CreateGroup(groupID string, description string) (*Group, err
 	if db.HasGroup(groupID) {
 		return nil, fmt.Errorf("%s: already registered group", groupID)
 	}
-	var group = Group{groupID, description, []string{}}
+	var group = Group{groupID, description}
 	db.Groups = append(db.Groups, group)
+
+	sort.Slice(db.Groups, func(i, j int) bool {
+		return db.Groups[i].Name < db.Groups[j].Name
+	})
 
 	return &group, nil
 }
@@ -151,6 +179,9 @@ func (db *Database) UpdateGroup(groupID string, newGroupID string, newDescriptio
 			db.Groups[i].Description = newDescription
 		}
 	}
+	sort.Slice(db.Groups, func(i, j int) bool {
+		return db.Groups[i].Name < db.Groups[j].Name
+	})
 
 	return true
 }
@@ -164,11 +195,10 @@ func (db *Database) Relate(groupID string, repoID string) error {
 	if db.HasRelation(groupID, repoID) {
 		return nil
 	}
-	for i, group := range db.Groups {
-		if group.Name == groupID {
-			db.Groups[i].Items = append(group.Items, repoID)
-		}
-	}
+	db.Relations = append(db.Relations, Relation{repoID, groupID})
+	sort.Slice(db.Relations, func(i, j int) bool {
+		return db.Relations[i].GroupName < db.Relations[j].GroupName
+	})
 
 	return nil
 }
@@ -178,12 +208,8 @@ HasRelation returns true if the group and the repository has relation.
 The group and the repository are specified by the given parameters.
 */
 func (db *Database) HasRelation(groupID string, repoID string) bool {
-	var group = db.FindGroup(groupID)
-	if group == nil {
-		return false
-	}
-	for _, item := range group.Items {
-		if item == repoID {
+	for _, relation := range db.Relations {
+		if relation.GroupName == groupID && relation.RepositoryID == repoID {
 			return true
 		}
 	}
@@ -199,21 +225,13 @@ func (db *Database) Unrelate(groupID string, repoID string) {
 	if !db.HasRelation(groupID, repoID) {
 		return
 	}
-	for i, group := range db.Groups {
-		if group.Name == groupID {
-			db.Groups[i].Items = db.removeItem(i, repoID)
+	var newRelations = []Relation{}
+	for _, relation := range db.Relations {
+		if !(relation.GroupName == groupID && relation.RepositoryID == repoID) {
+			newRelations = append(newRelations, relation)
 		}
 	}
-}
-
-func (db *Database) removeItem(index int, repoID string) []string {
-	var newItems = []string{}
-	for _, item := range db.Groups[index].Items {
-		if item != repoID {
-			newItems = append(newItems, item)
-		}
-	}
-	return newItems
+	db.Relations = newRelations
 }
 
 /*
@@ -282,9 +300,9 @@ func (db *Database) DeleteGroup(groupID string) error {
 	if !db.HasGroup(groupID) {
 		return fmt.Errorf("%s: group not found", groupID)
 	}
-	var group = db.FindGroup(groupID)
-	if len(group.Items) != 0 {
-		return fmt.Errorf("%s: group has %d relatins", groupID, len(group.Items))
+	var groups = groupFrequencies(db)
+	if groups[groupID] != 0 {
+		return fmt.Errorf("%s: group has %d relatins", groupID, groups[groupID])
 	}
 	var err = db.deleteGroup(groupID)
 	return err
@@ -334,7 +352,7 @@ How to call this function
 func Open(config *Config) (*Database, error) {
 	bytes, err := ioutil.ReadFile(databasePath(config))
 	if err != nil {
-		var db = Database{time.Now(), []Repository{}, []Group{}, config}
+		var db = Database{time.Now(), []Repository{}, []Group{}, []Relation{}, config}
 		return &db, nil
 	}
 
