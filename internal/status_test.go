5e00f86fa38ea4df073add1bc03399644124cb97
Haruaki Tamada <tamada@users.noreply.github.com>
2019-06-14 12:21:39 +0900

    update

--- internal/status_test.go
+++ internal/status_test.go
@@ -30,3 +30,22 @@ func TestStrftime(t *testing.T) {
 		}
 	}
 }
+
+func TestHelpOfStatusCommand(t *testing.T) {
+	var helpMessage = `rrh status [OPTIONS] [REPOSITORIES|GROUPS...]
+OPTIONS
+    -b, --branches               show the status of the local branches.
+    -r, --remote                 show the status of the remote branches.
+    -c, --csv                    print result in csv format.
+    -f, --time-format <FORMAT>   specifies time format. Available value is
+                                 'relative' ad 'absolute'
+ARGUMENTS
+    REPOSITORIES                 target repositories.  If no repository was specified
+                                 the command shows the result of the default group.
+    GROUPS                       target groups.  If no group was specified,
+                                 the command shows the result of the default group.`
+	var command, _ = StatusCommandFactory()
+	if command.Help() != helpMessage {
+		t.Errorf("help message did not match")
+	}
+}
