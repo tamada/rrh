32e82891dc7830f6d164c45c1c9c32ec10bb5f06
Haruaki Tamada <tamada@users.noreply.github.com>
2021-10-19 15:26:27 +0900

    update github actions for version up and publish

---
+++ .bin/update_version.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+VERSION=$1
+
+if [[ "$VERSION" == "" ]]; then
+    echo "no version specified"
+    exit 1
+fi
+
+result=0
+
+grep "$VERSION" Makefile 2>&1 > /dev/null || result=$?
+if [[ $result -eq 0 ]]; then
+    echo "already updated to $VERSION"
+    exit 1
+fi
+
+for i in README.md docs/content/_index.md; do
+    sed -e 's!Version-[0-9.]*-yellowgreen!Version-${VERSION}-yellowgreen!g' -e 's!tag/v[0-9.]*!tag/v${VERSION}!g' $i > a
+    mv a $i
+done
+
+sed 's/VERSION := .*/VERSION := ${VERSION}/g' Makefile > a && mv a Makefile
+sed 's/const VERSION = .*/const VERSION = "${VERSION}"/g' config.go > a && mv a config.go
+
+echo "Replace version to \"${VERSION}\""
--- Makefile
+++ Makefile
@@ -8,16 +8,7 @@ all: test build
 
 deps:
 
-update_version:
-	@for i in README.md docs/content/_index.md; do\
-	    sed -e 's!Version-[0-9.]*-yellowgreen!Version-${VERSION}-yellowgreen!g' -e 's!tag/v[0-9.]*!tag/v${VERSION}!g' $$i > a ; mv a $$i; \
-	done
-
-	@sed 's/const VERSION = .*/const VERSION = "${VERSION}"/g' config.go > a
-	@mv a config.go
-	@echo "Replace version to \"${VERSION}\""
-
-setup: deps update_version
+setup: deps
 	git submodule update --init
 
 test: setup
